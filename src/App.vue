558ad75e8078fe6f2c16a3bce5fe0822002904be
lexunok <lex48949@gmail.com>
2023-05-03 17:36:55 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HomePage/>
+  </div>
+</template>
+
+<script>
+import HomePage from './pages/HomePage.vue';
+export default{
+  components: {HomePage}
+}
+</script>
+
+<style scoped>
+
+</style>
